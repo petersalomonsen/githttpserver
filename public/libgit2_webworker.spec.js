cb2708f3c764f890cb7aab04255782dcbe77b84b
Peter Salomonsen <pjsalomonsen@gmail.com>
2020-11-01 11:32:59 +0100

    prevent scrolling on outer container

--- public/libgit2_webworker.spec.js
+++ public/libgit2_webworker.spec.js
@@ -1,14 +1,15 @@
-describe('libgit2 webworker', function() {
+describe('libgit2 webworker', function () {
     it.only('should be able to send token to the worker without waiting for ready message', async () => {
         const worker = new Worker('libgit2_webworker.js');
+        const replyPromise = new Promise(resolve =>
+            worker.onmessage = (msg) => resolve(msg)
+        );
         worker.postMessage({
             accessToken: 'token',
             username: 'abc',
             useremail: 'def@example.com'
         });
-        const msg = await new Promise(resolve =>
-            worker.onmessage = (msg) => resolve(msg)
-        );
+        const msg = await replyPromise;
         assert.isTrue(msg.data.accessTokenConfigured);
     });
 });
